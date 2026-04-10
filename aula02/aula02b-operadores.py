d73af71afc601a20cf0260657e0c872751a02c6c
RafaBoleta <rafa.a.rebello@gmail.com>
2026-04-09 21:02:44 -0300

    aula 02

---
+++ aula02/aula02b-operadores.py
@@ -0,0 +1,40 @@
+num1 = 4
+num2 = 2
+print(type(num1), type(num2))
+
+operacao = num1 / num2
+print(operacao, type(operacao))
+
+# OPERADOR DE ATRIBUIÇAO
+num = 22
+print() # pular uma linha
+print(num)
+
+num += 4
+print(num)
+
+# RELACIONAIS
+print()
+print(6 < 6)
+
+idade = 21
+print(idade >= 21)
+
+logado = True
+print(logado, type(logado))
+
+maior_idade = idade >= 18
+print(maior_idade)
+
+# STRINGS
+nome1 = "Marcos"
+nome2 = "marcos"
+
+print(nome1.upper() == nome2.upper())
+
+
+
+
+
+
+
